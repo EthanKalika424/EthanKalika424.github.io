e1b2af7ee9b856f4c8f0484b9019ca7a6eb89ee1
EthanKalika424 <97709280+EthanKalika424@users.noreply.github.com>
2022-02-19 09:41:26 -0500

    update

--- itis3135/scripts/navandfooter.js
+++ itis3135/scripts/navandfooter.js
@@ -1,8 +1,15 @@
-$(function () 
+/*$(function () 
 {
   $("#header").load("header.html");
 });
 $(function () 
 {
   $("#footer").load("footer.html");
-});
+});*/
+$(function () {
+var includes = $('[data-include]')
+$.each(includes, function () {
+  var file = 'components/' + $(this).data('header') + 'header.html'
+  $(this).load(file)
+})
+})
