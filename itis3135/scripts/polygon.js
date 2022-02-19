be25bb3308c2d004c4d225611ac2b495b57db395
EthanKalika424 <97709280+EthanKalika424@users.noreply.github.com>
2022-02-19 10:26:14 -0500

    polygon

---
+++ itis3135/scripts/polygon.js
@@ -0,0 +1,5 @@
+function alertUser()
+{
+    var user = prompt("The Emerald Kinkajou would like you to enter a number between 3-11 to tell you the polygon with that many sides you entered");
+    validateNum(user);
+}
