aa69ccab1399dfdc7dff50858c14072dbb6b5437
EthanKalika424 <97709280+EthanKalika424@users.noreply.github.com>
2022-02-19 11:17:08 -0500

    polygon

--- itis3135/scripts/polygon.js
+++ itis3135/scripts/polygon.js
@@ -1,5 +1,45 @@
 function alertUser()
 {
-    var user = prompt("The Emerald Kinkajou would like you to enter a number between 3-11 to tell you the polygon with that many sides you entered");
-    validateNum(user);
+    var num = prompt("The Emerald Kinkajou would like you to enter a number between 1-11 to tell you the polygon with that many sides you entered");
+    validateNum(num);
+}
+function validateNum(_num)
+{
+    if(isNaN(_num)){
+        alert("Sorry! Invalid number entry. Please type between 1-11!");
+        alertUser();
+    }
+    else
+    {
+        number = Math.trunc(_guess);
+        if(number > 11 || number < 1)
+        {
+            alert("Sorry! Invalid number entry. Please type between 1-11!");
+            alertUser();
+        }
+        else
+        {
+            getShape(number);
+        }
+    }
+}
+function getShape(sides)
+{
+    var polygonList = new Array(
+        "Monogon", 
+        "Bigon", 
+        "Triangle", 
+        "Quadrilateral", 
+        "Pentagon", 
+        "Hexagon", 
+        "Heptagon", 
+        "Octagon",
+        "Nonagon",
+        "Decagon",
+        "Undecagon"  
+    );
+    
+    var message = "A polygon with " + sides + " sides is called a " + polygonList[sides - 1] + ".";
+
+    document.getElementById("polygonName").innerHTML = message;
 }
