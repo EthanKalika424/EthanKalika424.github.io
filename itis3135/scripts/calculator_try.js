434e265679512b917370e4b8f713c1a7fb482a79
EthanKalika424 <97709280+EthanKalika424@users.noreply.github.com>
2022-02-26 14:00:38 -0500

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,11 +1,11 @@
-const calculator = document.querySelector('.calculator')
-//const keys = calculator.querySelector('.calculator__keys')
-const key = e.target
-const action = key.dataset.action
+let calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
 
 keys.addEventListener('click', e => {
  if (e.target.matches('button')) {
    // Do something
+   const key = e.target
+   const action = key.dataset.action
    if (!action) {
     console.log('number key!')
   }
