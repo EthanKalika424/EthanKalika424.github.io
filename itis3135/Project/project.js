a32f30e21ef8fee80714a14476d3b04456e8b94f
EthanKalika424 <97709280+EthanKalika424@users.noreply.github.com>
2022-05-01 08:16:28 -0400

    fix

--- itis3135/Project/project.js
+++ itis3135/Project/project.js
@@ -28,3 +28,14 @@ $(document).ready(function(){
         $(document).tooltip();
       });
 });
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      minSlides: 1,
+      maxSlides: 1,
+      slideWidth: 700,
+      adaptiveHeight: true,
+      captions: true,
+      speed: 1000,
+      auto: true
+    });
+  });
