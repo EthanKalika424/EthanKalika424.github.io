5675ea254366dfbe9d25b7a7e4467568fc055c44
EthanKalika424 <97709280+EthanKalika424@users.noreply.github.com>
2022-10-03 17:05:48 -0400

    index

--- MyPersonalSite/Flask/app.py
+++ MyPersonalSite/Flask/app.py
@@ -11,8 +11,8 @@ global studentOrganisationDetails
 @app.get('/')
 def index():
     # Complete this function to get current date and time assign this value to currentDate, display this data on index.html
-    
-    return render_template('index.html', currentDate=date.today)
+
+    return render_template('index.html', currentDate=datetime.datetime.now())
 
 
 @app.get('/calculate')
